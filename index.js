6037ab09f2a7ffc769403cf1e3a5a225dfc0143b
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-09-25 14:04:36 +0800

    original chat messaging

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 var app = require('express')();
-var http = require('http').createServer(app);
-var port = process.env.PORT||3000
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+var express = require('express');
+var users = []
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.on('chat message', function (msg) {
+    io.emit('chat message', msg);
   });
-http.listen(port,'0.0.0.0', function(){
-  console.log('listening on *:'+port);
+
+  socket.on('typing', function (typing) {
+    //io.emit('typing');
+    socket.broadcast.emit('typing', typing)
+
+  })
+
+  socket.on('online', function (names) {
+    if (!users.includes(names)) {
+      users.push(names);
+      io.emit("online", users)
+     // console.log(users)
+    } else {
+      io.emit("already_used", names+" already used");
+    }
+  });
+  socket.on('disconnect',function(data){
+    var i = users.indexOf(data);
+    users.splice(i,1);
+    io.emit('online',users);
+ })
+});
+app.use(express.static('public'));
+
+http.listen(port, function () {
+  console.log('listening on *:' + port);
 });
---
+++ public/script.js
@@ -0,0 +1,79 @@
+$("#SMS").hide();
+$(function () {
+    var names = $('#username').val();
+    var username = $('#username');
+    
+    //var listOfUser = [];
+    var socket = io();
+
+    socket.on('online', function (data) {
+        listOfUser = [];
+        $('.Us').remove()
+        for (let i = 0; i < data.length; i++) {
+           // console.log(listOfUser)
+            if (!listOfUser.includes(data[i]) && username.val() != data[i]) {
+                listOfUser.push(data[i]);
+                var user = data[i];
+                $('#activeUser').append($("<li class='Us'>").text(user));
+            }
+        }
+        console.log(listOfUser)
+    })
+
+    socket.on("already_used", function(data){
+        alert(data);
+    })
+
+
+    $('#submitButton').click(function () {
+        //$('#activeUser').append("<li>"+username.val());
+        $("#title").text(username.val());
+        socket.emit('online', username.val());
+       // console.log(username.val());
+        $('#userinterface').hide();
+        $("#SMS").show();
+        $("#disconnect").click(function () {
+            socket.emit('disconnect', username.val());
+            location.reload();
+          })
+
+    })
+    $('form').submit(function () {
+        socket.emit('chat message', username.val() + " : " + $('#m').val());
+        $('#m').val('');
+        return false;
+    });
+
+    $('#m').on('keypress', function () {
+        socket.emit('typing', username.val());
+    })
+
+
+    socket.on('typing', function (msg, err) {
+      //  console.log(err);
+        $('#typing').html(msg + " is typing a message...");
+        setTimeout(function () {
+            $("#typing").html('');
+        }, 2000);
+    })
+
+
+
+    socket.on('offline', function () {
+
+    })
+    socket.on('chat message', function (msg) {
+        // $('#messages').append($('<li>').text(msg));
+        console.log(msg.split(" : ")[0])
+        if (username.val() == msg.split(" : ")[0]) {
+            $('#messages').append($('<li style="padding:5px;border-radius:10px;background-color:deepskyblue;margin-left:340px;margin-top:30px;">').text(msg));
+        } else {
+            $('#messages').append($('<li style="padding:5px;border-radius:10px;background-color:white;color:deepskyblue;margin-right:340px;margin-top:30px; ">').text(msg));
+        }
+    });
+
+
+});
+window.scrollTo(0, document.body.scrollHeight);
+
+
