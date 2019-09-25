ff9f57f92e1a84e371f41ed43588622848e3788c
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-09-25 13:57:46 +0800

    change to passerellesnumeriques

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var port = process.env.PORT||3000
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
-});
-
+    res.sendFile(__dirname + '/index.html');
+  });
 http.listen(port,'0.0.0.0', function(){
   console.log('listening on *:'+port);
 });
